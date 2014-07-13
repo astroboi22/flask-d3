76f81474c32a2994169a36f936f45c31842516ff
astroboi22 <andrew.p.chiang@gmail.com>
2014-07-13 13:22:51 -0500

    Trying to set up Flask Dashboard

---
+++ app.py
@@ -0,0 +1,57 @@
+"""
+This file is part of the flask+d3 Hello World project.
+"""
+import json
+import flask
+import numpy as np
+
+
+app = flask.Flask(__name__)
+
+
+@app.route("/blah")
+def blech():
+    dude = "blah"
+    return dude
+
+@app.route("/test.html")
+def testTable():
+    return flask.render_template("test.html")
+
+@app.route("/morrischarts")
+def morris():
+    return flask.render_template("morris.html")
+
+@app.route("/")
+def index():
+    """
+    When you request the root path, you'll get the index.html template.
+
+    """
+    return flask.render_template("index.html")
+
+
+@app.route("/data")
+@app.route("/data/<int:ndata>")
+def data(ndata=100):
+    """
+    On request, this returns a list of ``ndata`` randomly made data points.
+
+    :param ndata: (optional)
+        The number of data points to return.
+
+    :returns data:
+        A JSON string of ``ndata`` data points.
+
+    """
+    x = 10 * np.random.rand(ndata) - 5
+    y = 0.5 * x + 0.5 * np.random.randn(ndata)
+    A = 10. ** np.random.rand(ndata)
+    c = np.random.rand(ndata)
+    return json.dumps([{"_id": i, "x": x[i], "y": y[i], "area": A[i],
+        "color": c[i]}
+        for i in range(ndata)])
+
+
+app.debug = True
+app.run(host="0.0.0.0", port=5050)
